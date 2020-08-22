35d61947858f50216fa224084d790904a669aeef
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-22 11:23:41 -0500

    added some more formatting

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -12,24 +12,107 @@ namespace MethodExercise
             //What city do you live in?
             //To get acceptable input, you must guide the user
 
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+
+
+
+
+            Console.WriteLine("Good day, human. Please, tell me about yourself!");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("=================================================");
+
             Console.WriteLine("What is your name?");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+
             var userName = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
 
             Console.WriteLine($"Hey there, {userName}! What is a band that you like?");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
             var likedBand = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+
+
+
+
+
 
             Console.WriteLine($"Great! {likedBand} ROCKS SO HARD! Can you tell me a movie you like?");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+
             var likedMovie = Console.ReadLine();
 
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+
+
             Console.WriteLine($"{likedMovie}?! {userName}, you have really great taste. \nI like that one a lot too.\nWhere do you stay at?");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+
+
             var dwelling = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
 
             Console.WriteLine($"Yikes. {dwelling} sucks.");
 
-            Console.WriteLine($"Name: {userName}");
-            Console.WriteLine($"Liked Band: {likedBand}");
-            Console.WriteLine($"Liked Movie: {likedMovie}");
-            Console.WriteLine($"Dwells in: {dwelling}");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine($"Thanks, {userName}! Here is what I've learned about you:");
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+
+
+
+
+            Console.WriteLine($"You're name is {userName}");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine($"You enjoy listening to {likedBand}");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine($"You like the movie {likedMovie}");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine($"You live in {dwelling}");
+
+
+
+
+
+
+
+            
 
 
         }
