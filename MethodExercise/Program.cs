caa6c76bafb440fc2304a18a598113e9ff491b99
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-21 20:09:21 -0500

    Completed first part of exercise

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -10,7 +10,7 @@ namespace MethodExercise
             //What is a movie you like?
             //What is a band that you like?
             //What city do you live in?
-            //To get acceptable input, you must guide that user
+            //To get acceptable input, you must guide the user
 
             Console.WriteLine("What is your name?");
             var userName = Console.ReadLine();
@@ -18,10 +18,10 @@ namespace MethodExercise
             Console.WriteLine($"Hey there, {userName}! What is a band that you like?");
             var likedBand = Console.ReadLine();
 
-            Console.WriteLine($"Great! I like {likedBand} as well! Can you tell me a movie you like?");
+            Console.WriteLine($"Great! {likedBand} ROCKS SO HARD! Can you tell me a movie you like?");
             var likedMovie = Console.ReadLine();
 
-            Console.WriteLine($"{likedMovie}?! I like that one too, where do you stay at?");
+            Console.WriteLine($"{likedMovie}?! {userName}, you have really great taste. I like that one a lot too, where do you stay at?");
             var dwelling = Console.ReadLine();
 
             Console.WriteLine($"Yikes. {dwelling} sucks.");
